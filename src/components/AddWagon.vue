4b96e7b4739cc4a91f04d15ffe3afe5fb91fb380
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2021-02-10 18:34:48 +0100

    add delete wagon

--- src/components/AddWagon.vue
+++ src/components/AddWagon.vue
@@ -1,28 +1,33 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
-  <div>
+  <div class="flex justify-start space-x-4 place-items-center">
     <label for="wagon">Wpisz numer wagonu</label>
-    <span class="text-red-400" v-if="wrongNumber"
-      >Wpisałeś za krótki nr wagonu lub próbujesz dodać ponownie ten sam
-      wagon</span
-    >
     <input
       @change="wrongNumber = false"
       @keyup.enter="addWagon"
-      class="text-black"
+      class="text-black h-12 text-lg placeholder-gray-400"
       type="number"
       id="wagon"
       ref="wagonInput"
       maxlength="12"
+      placeholder="nr wagonu"
     />
     <button @click="addWagon">Dodaj</button>
+    <span class="text-red-400" v-show="wrongNumber"
+      >Wpisałeś za krótki nr wagonu lub próbujesz dodać ponownie ten sam
+      wagon</span
+    >
   </div>
   <div>
-    <ul>
-      <li v-for="(wagon, index) in wagons" :key="index">
+    <ul class="flex flex-col w-1/3 my-8">
+      <li
+        class="flex justify-around border-gray-400 border-2"
+        v-for="(wagon, index) in wagons"
+        :key="index"
+      >
+        <span>Nr:{{ index + 1 }}</span>
+
         {{ wagon.wagon }}
+        <button class="text-red-400" @click="deleteWagon(index)">Usuń</button>
       </li>
     </ul>
   </div>
@@ -54,8 +59,10 @@ export default {
       } else {
         this.wagons.push({ wagon: wagonNumber });
         this.$refs.wagonInput.value = "";
-        console.log(this.wagons);
       }
+    },
+    deleteWagon(id) {
+      this.wagons.splice(id, 1);
     },
   },
 };
