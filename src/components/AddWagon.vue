df9e01d5e066805526f2d168d0c3823d27153ddd
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2021-02-10 19:52:30 +0100

    adding wagontype

--- src/components/AddWagon.vue
+++ src/components/AddWagon.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="flex justify-start space-x-4 place-items-center">
-    <label for="wagon">Wpisz numer wagonu</label>
-    <input
-      @change="wrongNumber = false"
-      @keyup.enter="addWagon"
-      class="text-black h-12 text-lg placeholder-gray-400"
-      type="number"
-      id="wagon"
-      ref="wagonInput"
-      maxlength="12"
-      placeholder="nr wagonu"
-    />
-    <button @click="addWagon">Dodaj</button>
+    <div class="border-2 flex space-x-6">
+      <label for="type">Typ wagonu:</label>
+      <select ref="type" id="type" class="text-black">
+        <option value="kgns">Kgns</option>
+        <option value="sgs" selected>Sgs</option>
+        <option value="sggrss">Sggrss</option>
+        <option value="rgmms">Rgmms</option>
+        <option value="sdgmnss">Sdgmnss</option>
+      </select>
+    </div>
+    <div class="border-2 flex place-items-center space-x-6">
+      <label for="wagon">Wpisz numer wagonu</label>
+      <input
+        @input="wrongNumber = false"
+        @keyup.enter="addWagon"
+        class="text-black h-12 text-lg placeholder-gray-400"
+        maxlength="12"
+        id="wagon"
+        ref="wagonInput"
+        placeholder="nr wagonu"
+      />
+    </div>
+    <button class="rounded-lg border-2 px-4" @click="addWagon">Dodaj</button>
     <span class="text-red-400" v-show="wrongNumber"
       >Wpisałeś za krótki nr wagonu lub próbujesz dodać ponownie ten sam
       wagon</span
@@ -25,9 +36,15 @@
         :key="index"
       >
         <span>Nr:{{ index + 1 }}</span>
+        <span>Typ: {{ wagon.wagonType }}</span>
 
-        {{ wagon.wagon }}
-        <button class="text-red-400" @click="deleteWagon(index)">Usuń</button>
+        {{ wagon.wagonNumber }}
+        <button
+          class="rounded-lg border-2 px-4 text-red-400"
+          @click="deleteWagon(index)"
+        >
+          Usuń
+        </button>
       </li>
     </ul>
   </div>
@@ -48,6 +65,7 @@ export default {
   methods: {
     addWagon() {
       const wagonNumber = this.$refs.wagonInput.value;
+      const wagonType = this.$refs.type.value;
       const wagonNormalLength = 12;
 
       if (
@@ -55,10 +73,12 @@ export default {
         this.wagons.some(({ wagon }) => (wagon === wagonNumber ? true : false))
       ) {
         this.wrongNumber = true;
+        console.log(this.$refs.type.value);
         return;
       } else {
-        this.wagons.push({ wagon: wagonNumber });
+        this.wagons.push({ wagonNumber, wagonType });
         this.$refs.wagonInput.value = "";
+        console.log(this.wagons);
       }
     },
     deleteWagon(id) {
